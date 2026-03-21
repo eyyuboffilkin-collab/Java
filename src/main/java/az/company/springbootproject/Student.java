647a94d1d229f089bb73f4fa51b6a6199a368756
Ilkin <eyyuboffilkin@gmail.com>
2026-03-21 20:26:46 +0400

    Update Student.java

--- src/main/java/az/company/springbootproject/Student.java
+++ src/main/java/az/company/springbootproject/Student.java
@@ -5,10 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
-<<<<<<< HEAD
+
 import org.springframework.web.bind.annotation.GetMapping;
-=======
->>>>>>> 905084f (Student Hub)
+
 @Data
 @Entity
 public class Student {
